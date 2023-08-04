628d45c35f3eb5d5d45108048490fbc25352b278
wagner-programer <134347588+wagner-programer@users.noreply.github.com>
2023-08-04 01:53:44 -0300

    teste JS

---
+++ exercicios-js/index.js
@@ -0,0 +1,17 @@
+//teste java script
+/*
+autor:Wagner Oliveira
+*/
+//document.getElementById("texto").innerHTML = "Meu primeiro teste de <strong>JS</strong>";
+
+//declaração de variaveis
+
+var nome, sobrenome, nomeCompleto;
+
+//Atribuição dos valores
+
+nome = "Wagner";
+sobrenome = "Oliveira";
+nomeCompleto = nome + "  "+ sobrenome;
+
+document.getElementById("texto").innerHTML = nomeCompleto;
