4090568a1d00cf68117322fb41fef20efbb4c33b
Wagner Oliveira <wa.oliveira23@gmail.com>
2023-09-01 01:47:13 -0300

    exercicios JS

---
+++ .vscode/JS/arrays e vetores/array.js
@@ -0,0 +1,19 @@
+//arrays pode guardar todo tipo de dados
+
+let array = ['String',1, true, ['array1'], ['array2']];
+console.log(array);
+
+//Foreach
+
+/*Array.forEach(function(item,index) {console.log(item,index)});*/
+
+/*array.push('novo dado');
+console.log(array);*/
+
+//array.pop(); console.log(array);
+
+//array.shift(); console.log(array);
+
+//array.unshift('novo item no inicio'); console.log(array);
+
+console.log(array.indexOf (1));
